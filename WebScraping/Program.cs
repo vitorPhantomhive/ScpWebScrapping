5bb3812d33688df544aa824411fdf349905ab63b
Vitor <vitorkhaled47@gmail.com>
2024-06-17 23:25:47 -0300

    configurando para receber os scps
    ADicionado mapeamento dos itens nas telas, ainda nao esta completo, falta algumas partes

--- WebScraping/Program.cs
+++ WebScraping/Program.cs
@@ -1,16 +1,11 @@
-﻿using CsvHelper;
-using HtmlAgilityPack;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
+﻿using HtmlAgilityPack;
 using WebScraping;
 
 namespace StaticWebScraping
 {
     public class Program
     {
-        public static async void Main()
+        public static async Task Main()
         {
 
             //URL da página alvo
@@ -37,15 +32,41 @@ namespace StaticWebScraping
                 //Inicializandoa lista de objetos que armazenará os dados raspados
                 List<SCP> scpItems = new List<SCP>();
 
-                var item = new SCP();
-
-                var itemNumberNode = pageContentNode.SelectSingleNode(".//p[strong[contains(text(), Item nº)]]");
+                var itemScp = new SCP();
+                //pegando os valores do html 
+                var itemNumberNode = pageContentNode.SelectSingleNode(".//p[strong[contains(text(),'Item nº')]]");
                 if(itemNumberNode !=  null)
                 {
-                    item.ItemNumber = itemNumberNode.InnerText.Replace("Item nº", "").Trim();
+                    itemScp.ItemNumber = itemNumberNode.InnerText.Replace("Item nº", "").Trim();
                 }
+                var itemObjectClass = pageContentNode.SelectSingleNode(".//p[strong[contains(text(),'Classe do Objeto')]]");
+                if(itemObjectClass != null)
+                {
+                    itemScp.ObjectClass = itemObjectClass.InnerText.Replace("Classe do Objeto", "");
+                }
+                // Selecionar o nó do cabeçalho "Procedimentos Especiais de Contenção"
+                var proceduresHeaderNode = pageContentNode.SelectSingleNode(".//p[strong[contains(text(), 'Procedimentos Especiais de Contenção:')]]");
 
-                Console.WriteLine(itemNumberNode);
+                if (proceduresHeaderNode != null)
+                {
+                    var currentNode = proceduresHeaderNode.NextSibling;
+                    while (currentNode != null && !(currentNode.Name == "p" && currentNode.InnerText.Contains("Descrição:")))
+                    {
+                        if (currentNode.Name == "p")
+                        {
+                            itemScp.ContainmentProcedures += currentNode.InnerText + "\n";
+                        }
+                        currentNode = currentNode.NextSibling;
+                    }
+                }
+                scpItems.Add(itemScp);
+                foreach (var scp in scpItems)
+                {
+                    Console.WriteLine("Item Number: " + scp.ItemNumber);
+                    Console.WriteLine("Classe do Objeto: " + scp.ObjectClass);
+                    Console.WriteLine("Procedimentos Especiais de Contenção: " + scp.ContainmentProcedures);
+                    // Você pode adicionar mais exibições aqui para ObjectClass, ContainmentProcedures e Description se necessário
+                }
 
             }
         }
