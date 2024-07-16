8081346839d43ec8791c12a5fcacd8911e4c9a67
Vitor <vitorkhaled47@gmail.com>
2024-07-15 22:30:31 -0300

    fix: corrigindo inserts e comentarios

--- WebScraping/Program.cs
+++ WebScraping/Program.cs
@@ -1,8 +1,7 @@
 ﻿using HtmlAgilityPack;
-using WebScraping;
 using Newtonsoft.Json;
 using System.Net.Http.Json;
-
+using WebScraping;
 
 namespace StaticWebScraping
 {
@@ -11,17 +10,16 @@ namespace StaticWebScraping
         public static async Task Main()
         {
             await Start();
-
         }
 
         public static async Task Start()
         {
-            //Inicializandoa lista de objetos que armazenará os dados raspados
+            // Inicializando a lista de objetos que armazenará os dados raspados
             List<SCP> scpItems = new List<SCP>();
-            var itemScp = new SCP();
+
             for (int v = 99; v < 3000; v++)
             {
-                //URL da página alvo
+                // URL da página alvo
                 string url = $"http://scp-pt-br.wikidot.com/scp-{v}";
                 using (HttpClient client = new HttpClient())
                 {
@@ -29,46 +27,45 @@ namespace StaticWebScraping
                     //response.EnsureSuccessStatusCode();
                     string pageContent = await response.Content.ReadAsStringAsync();
 
-                    //carregar o conteúdo html na instancia de htmlDocument
+                    // Carregar o conteúdo html na instancia de htmlDocument
                     HtmlDocument document = new HtmlDocument();
                     document.LoadHtml(pageContent);
 
-                    //selecionando o nó html de interesse
+                    // Selecionando o nó html de interesse
                     var pageContentNode = document.DocumentNode.SelectSingleNode("//*[@id='page-content']");
 
+                    // Criando um novo objeto SCP
+                    var itemScp = new SCP();
 
-
-                    //pegando os valores do html 
+                    // Pegando os valores do html 
                     var itemNumberNode = pageContentNode.SelectSingleNode(".//p[strong[contains(text(),'Item nº')]]");
-
-                    if (itemNumberNode == null)
-                    {
-                        Console.WriteLine($"Não foi possivel encontrar o scp: {v}");
-                        continue;
-                    }
-
                     if (itemNumberNode != null)
                     {
-                        itemScp.ItemNumber = itemNumberNode.InnerText.Replace("Item nº", "").Trim();
+                        itemScp.ItemNumber = itemNumberNode.InnerText.Replace("Item nº", "").Trim().Replace(":", "").Replace("SCP-", "").Trim();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Não foi possível encontrar o SCP: {v}");
+                        continue;
                     }
 
                     var itemObjectClass = pageContentNode.SelectSingleNode(".//p[strong[contains(text(),'Classe do Objeto')]]");
                     if (itemObjectClass != null)
                     {
-                        itemScp.ObjectClass = itemObjectClass.InnerText.Replace("Classe do Objeto", "");
+                        itemScp.ObjectClass = itemObjectClass.InnerText.Replace("Classe do Objeto", "").Trim().Replace(":", "").Trim();
                     }
 
                     // Selecionar o nó do cabeçalho "Procedimentos Especiais de Contenção"
                     var proceduresHeaderNode = pageContentNode.SelectSingleNode(".//p[strong[contains(text(),'Procedimentos Especiais de Contenção')]]");
                     if (proceduresHeaderNode != null)
                     {
-                        //Criando uma lista de procedimentos
+                        // Criando uma lista de procedimentos
                         List<string> containmentProcedures = new List<string>();
 
-                        //ADicionar o primeiro parágrafo (cabeçalho) sem o texto "Procedimentos de contenção"
-                        containmentProcedures.Add(proceduresHeaderNode.InnerText.Replace("Procedimentos Especiais de Contenção", "").Trim());
+                        // Adicionar o primeiro parágrafo (cabeçalho) sem o texto "Procedimentos de contenção"
+                        containmentProcedures.Add(proceduresHeaderNode.InnerText.Replace("Procedimentos Especiais de Contenção", "").Trim().Replace(":", "").Trim());
 
-                        //Selecionar todos os nós seguintes até encontrar outro cabeçalho ou fim do conteúdo
+                        // Selecionar todos os nós seguintes até encontrar outro cabeçalho ou fim do conteúdo
                         var nextSibling = proceduresHeaderNode.NextSibling;
                         while (nextSibling != null && nextSibling.Name != "strong")
                         {
@@ -79,29 +76,28 @@ namespace StaticWebScraping
                             }
                             if (nextSibling.Name == "p" || nextSibling.Name == "#text")
                             {
-                                containmentProcedures.Add((string)nextSibling.InnerText.Trim());
+                                containmentProcedures.Add(nextSibling.InnerText.Trim());
                             }
                             nextSibling = nextSibling.NextSibling;
                         }
                         // Combinar todos os parágrafos em uma única string
-                        itemScp.ContainmentProcedures = string.Join("", containmentProcedures).Replace("\r\n", "");
-                        //itemScp.ContainmentProcedures = proceduresHeaderNode.InnerText.Replace("Procedimentos Especiais de Contenção", "");
+                        itemScp.ContainmentProcedures = string.Join("", containmentProcedures).Replace("\r\n", "").Replace(":", "").Trim();
                     }
 
                     var itemDescription = pageContentNode.SelectSingleNode(".//p[strong[contains(text(),'Descrição')]]");
                     if (itemDescription != null)
                     {
                         List<string> descriptions = new List<string>();
-                        itemScp.Description = itemDescription.InnerText.Replace("Descrição", "");
+                        descriptions.Add(itemDescription.InnerText.Replace("Descrição", "").Trim().Replace(":", "").Trim());
 
-                        //selecionar todos os nós seguintes até encontrar outro cabeçalho ou acabar
+                        // Selecionar todos os nós seguintes até encontrar outro cabeçalho ou acabar
                         var nextSibling = itemDescription.NextSibling;
                         while (nextSibling != null && nextSibling.Name != "div")
                         {
                             // Verificar se o nó atual é um parágrafo sem tag <strong> dentro dele
                             if (nextSibling.Name == "p" && nextSibling.SelectSingleNode(".//strong") == null)
                             {
-                                descriptions.Add((string)nextSibling.InnerText.Trim());
+                                descriptions.Add(nextSibling.InnerText.Trim());
                             }
                             else if (nextSibling.Name == "p" || nextSibling.SelectSingleNode(".//strong") != null)
                             {
@@ -109,37 +105,33 @@ namespace StaticWebScraping
                             }
                             nextSibling = nextSibling.NextSibling;
                         }
-                        itemScp.Description = string.Join("", descriptions).Replace("\r\n", "");
+                        itemScp.Description = string.Join("", descriptions).Replace("\r\n", "").Replace(":", "").Trim();
                     }
+
                     scpItems.Add(itemScp);
 
-                }
+                    // Indicando onde eu quero o caminho do arquivo
+                    string path = "C:\\Users\\vitor\\source\\repos\\WebScraping\\WebScraping\\json_scp\\json_scp.json";
 
+                    // Transformando em json
+                    string json = JsonConvert.SerializeObject(scpItems);
 
-                //indicando onde eu quero o caminho do arquivo
-                string path = "C:\\Users\\vitor\\source\\repos\\WebScraping\\WebScraping\\json_scp\\json_scp.json";
+                    // Criando o arquivo
+                    File.WriteAllText(path, json);
 
-                //trasnformando em json
-                string json = JsonConvert.SerializeObject(itemScp);
-
-                //criando o arquivo
-                File.WriteAllText(path, json);
-
-
-                string url_api = "https://localhost:7190/api/Scp";
-                HttpClient client_post = new HttpClient();
-                var postResponse = await client_post.PostAsJsonAsync(url_api, itemScp);
-                if (postResponse.IsSuccessStatusCode)
-                {
-                    Console.WriteLine($"Requisição feita com sucesso para o SCP: {v}");
-                }
-                else
-                {
-                    Console.WriteLine($"Falha na requisição para o SCP: {v}, Status: {postResponse.StatusCode}");
+                    string url_api = "https://apiscp.azurewebsites.net/api/Scp";
+                    HttpClient client_post = new HttpClient();
+                    var postResponse = await client_post.PostAsJsonAsync(url_api, itemScp);
+                    if (postResponse.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine($"Requisição feita com sucesso para o SCP: {v}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Falha na requisição para o SCP: {v}, Status: {postResponse.StatusCode}");
+                    }
                 }
             }
-
         }
     }
-
 }
